156e85afd408ebee0336d5308d4bf235315a0f05
nilsth <nilsth@192.168.2.6>
2018-03-05 20:24:26 +0100

    @ExecutionMode(mode=ExecutionModeType.EXECUTE_METHOD_DIRECTLY) for viewProjectTemplate(ProjectTemplate projectTemplate)

--- MeynControlsConfigurator/src/nth/meyn/controls/configurator/dom/project/ProjectTemplateService.java
+++ MeynControlsConfigurator/src/nth/meyn/controls/configurator/dom/project/ProjectTemplateService.java
@@ -3,6 +3,9 @@ package nth.meyn.controls.configurator.dom.project;
 import java.util.ArrayList;
 import java.util.List;
 
+import nth.introspect.layer5provider.reflection.behavior.executionmode.ExecutionMode;
+import nth.introspect.layer5provider.reflection.behavior.executionmode.ExecutionModeType;
+
 public class ProjectTemplateService {
 
 	public List<ProjectTemplate> allProjectTemplates() {
@@ -11,6 +14,7 @@ public class ProjectTemplateService {
 		return projectTemplates;
 	}
 
+	@ExecutionMode(mode=ExecutionModeType.EXECUTE_METHOD_DIRECTLY)
 	public ProjectTemplate viewProjectTemplate(ProjectTemplate projectTemplate) {
 		return projectTemplate;
 	}
