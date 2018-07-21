ec1d87862cbaf845a2af796b176bd04534d7f503
nilsth <nilsth@192.168.2.3>
2018-07-21 12:38:07 +0200

    First commit

---
+++ PlayListCreatorForJavaFx/src/main/java/ReadMe.java
@@ -0,0 +1,26 @@
+/**
+ * See 
+ * 
+ * Youtube
+ * e.g. https://www.youtube.com/watch_videos?video_ids=sjRTSkBHnyo,fhU5y7huAY8,....
+ * https://www.labnol.org/internet/create-youtube-playlists/28827/
+ * 
+ * https://www.tastekid.com/read/api
+ * https://www.tastekid.com/api/similar?q=7 nation army&k=224813-unknowna-K6ZXO3K3&info=1&type=music
+ * 
+ * freedb
+ * http://www.codeproject.com/Articles/7389/Retrieving-CD-Information-from-a-remote-Freedb-dat
+ * 
+ * music db:
+ * http://musicbrainz.org/doc/Development/XML_Web_Service/Version_2
+ * https://code.google.com/archive/p/musicbrainzws2-java/
+ * 
+ * 
+ * 
+ * 
+ * @author nilsth
+ *
+ */
+public class ReadMe {
+
+}
